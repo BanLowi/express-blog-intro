4766ff69c2aa38751165bc93093a8243b8991c81
BanLowi <lowi40k@gmail.com>
2025-11-24 12:41:18 +0100

    send an image in /bacheca

--- server.js
+++ server.js
@@ -2,9 +2,27 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.use(express.static("public"));
+
+const posts = [
+    {
+        title: "Ciambellone",
+        content: "Un bellissimo ciambellone caldo",
+        image: "images/ciambellone.jpeg",
+        tags: [
+            "buono", "grasso"
+        ]
+    }
+]
+
 app.get("/", (req, res) => {
 
-    res.json("Server del mio blog")
+    res.send("Server del mio blog");
+})
+
+app.get("/bacheca", (req, res) => {
+
+    res.send(`<img src="/images/ciambellone.jpeg">`);
 })
 
 app.listen(port, () => {
