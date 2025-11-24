d849181db64832ac192d947fd6cf248f978f96f7
BanLowi <lowi40k@gmail.com>
2025-11-24 12:14:45 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+
+    res.json("Server del mio blog")
+})
+
+app.listen(port, () => {
+    console.log(`This app is listening on ${port}`);
+})
